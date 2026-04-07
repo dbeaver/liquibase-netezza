06f80932c7ef5af5e04c6acd15ec0818cf0ce817
Georgii <georgii.gvinepadze@dbeaver.com>
2026-04-07 17:25:41 +0400

    dbeaver/pro#8539 license
    Signed-off-by: Georgii Gvinepadze <fireshowel@gmail.com>

--- src/main/java/liquibase/ext/netezza/change/ModifyDataTypeChangeDestructiveNetezza.java
+++ src/main/java/liquibase/ext/netezza/change/ModifyDataTypeChangeDestructiveNetezza.java
@@ -1,13 +1,26 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2027 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package liquibase.ext.netezza.change;
 
 import liquibase.change.DatabaseChange;
 import liquibase.change.core.ModifyDataTypeChange;
 import liquibase.database.Database;
-import liquibase.database.core.DB2Database;
 import liquibase.ext.netezza.statement.ModifyColumnDataTypeStatementNetezza;
 import liquibase.statement.SqlStatement;
-import liquibase.statement.core.ModifyDataTypeStatement;
-import liquibase.statement.core.ReorganizeTableStatement;
 
 /**
  * Netezza only allows changing length(by incrementing but not decrementing) and precision(by incrementing but not decrementing) of the data type.
